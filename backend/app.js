7ca9fad1bcc2b3504031486a0237cde102e4d3eb
Diego26Rodriguez <diegofactorial@gmail.com>
2023-11-26 22:15:39 -0500

    cambio4

--- backend/app.js
+++ backend/app.js
@@ -19,5 +19,5 @@ app.use('/productos', productRouter);
 app.use('/facturas', facturaRouter);
 
 const PORT = process.env.PORT || 3333;
-console.log('PORT: ' + PORT);
 app.listen(PORT);
+console.log('PORT: ' + PORT);
