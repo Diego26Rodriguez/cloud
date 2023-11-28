e70e43b555abc1faadf5906d8b1cb72a1357c902
Diego26Rodriguez <diegofactorial@gmail.com>
2023-11-27 23:46:44 -0500

    cambio11

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,6 @@ const thirdRouter = require('./routers/tercerosRouter')
 const productRouter = require('./routers/productosRouter')
 const facturaRouter = require('./routers/facturasRouter')
 
-
 const app = express()
 app.use(bodyParser.json())
 
@@ -17,8 +16,9 @@ app.use('/terceros', thirdRouter);
 app.use('/productos', productRouter);
 app.use('/facturas', facturaRouter);
 
-const puerto = 3333;  // Cambia 3334 por el número de puerto que desees
+const puerto = 3333;
+const direccionIP = '18.212.242.25';  // Reemplaza con tu dirección IP pública
+
 app.listen(puerto, direccionIP, () => {
-    
   console.log(`Servidor escuchando en http://${direccionIP}:${puerto}`);
 });
